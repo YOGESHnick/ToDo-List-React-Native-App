9a7126a6200f7d34805c243e7a0cbdf69b4a6bc7
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-07-22 13:13:00 +0530

    added feature to delete items

--- App.js
+++ App.js
@@ -16,13 +16,24 @@ export default function App() {
     );
   };
 
+  function deleteGoalHandler(id){
+    setGoals(currentGoals=>{
+      return currentGoals.filter( (goal) => goal.id!==id );
+    })
+  }
+
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler}/>
       <View style={styles.goalsContainer}>
         <FlatList data={goals} 
         renderItem={itemData=>{
-          return <GoalItem text={itemData.item.text}/>;
+          return (
+          <GoalItem 
+          text={itemData.item.text} 
+          id={itemData.item.id}
+          onDeleteItem={deleteGoalHandler}/>
+          );
         }}
         keyExtractor={(item,index)=>{
           return item.id;
